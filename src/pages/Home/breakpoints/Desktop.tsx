b929ce2e6e939ccbb88f106f63019f5931be5267
Rafael Fontes <itsranked.net@gmail.com>
2021-04-12 22:07:43 -0300

    Add banners

--- src/pages/Home/breakpoints/Desktop.tsx
+++ src/pages/Home/breakpoints/Desktop.tsx
@@ -11,80 +11,103 @@ import useScores from 'src/hooks/useScores';
 import './desktop.css';
 import './index.css';
 
+const banners = [
+  'banner.webp',
+  'banner1.jpg',
+  'banner2.jpg',
+  'banner3.gif',
+  'banner4.png',
+  'banner5.gif',
+  'banner6.gif',
+  'banner7.jpg',
+];
+
+function getRandomBanner() {
+  return banners[Math.floor(Math.random() * banners.length)];
+}
+
 function HomePageDesktop() {
-    const scores = useScores();
-    const { _ } = useInternationalization();
+  const scores = useScores();
+  const { _ } = useInternationalization();
 
-    return (
-        <div id="container">
-            <Header />
-            <NavBar />
+  return (
+    <div id="container">
+      <Header />
+      <NavBar />
 
-            <div id="content">
-                <div className="ad left-side">
-                    <div>
-                        {[...new Array(12)].map((_, index) => (
-                            <div key={index} className="banner"></div>
-                        ))}
-                    </div>
+      <div id="content">
+        <div className="ad left-side">
+          <div>
+            {[...new Array(12)].map((_, index) => (
+              <div key={index} className="banner">
+                <img src={`/${getRandomBanner()}`} alt="" />
+              </div>
+            ))}
+          </div>
 
-                    <div>
-                        {[...new Array(12)].map((_, index) => (
-                            <div key={index} className="banner"></div>
-                        ))}
-                    </div>
-                </div>
-                <ul>
-                    {scores.map((score) => (
-                        <li key={score.position}>
-                            {score.position < 4 ? (
-                                <div className={`medal position-${score.position}`}>
-                                    <FontAwesomeIcon icon={faMedal} />
-                                </div>
-                            ) : (
-                                <div className="position">#{score.position}</div>
-                            )}
-                            <div className="info first-column">
-                                <span className="username">{score.userName}</span>
-                                <span className="server">
-                                    <span className="address">{score.server}</span>
-                                    <ServerFlag server={score.server} />
-                                </span>
-                            </div>
-                            <div className="info second-column">
-                                <span>{_('content.info.score')}</span>
-                                <span>{score.score}</span>
-                            </div>
-                            <div className="info third-column">
-                                <span>
-                                    <Moment format={_('content.info.day-format')}>{score.timestamp}</Moment>
-                                </span>
-                                <span>
-                                    <Moment format={_('content.info.time-format')}>{score.timestamp}</Moment>
-                                </span>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="ad right-side">
-                    <div>
-                        {[...new Array(12)].map((_, index) => (
-                            <div key={index} className="banner"></div>
-                        ))}
-                    </div>
-
-                    <div>
-                        {[...new Array(12)].map((_, index) => (
-                            <div key={index} className="banner"></div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-            <StatusBar />
+          <div>
+            {[...new Array(12)].map((_, index) => (
+              <div key={index} className="banner">
+                <img src={`/${getRandomBanner()}`} alt="" />
+              </div>
+            ))}
+          </div>
         </div>
-    );
+        <ul>
+          {scores.map((score) => (
+            <li key={score.position}>
+              {score.position < 4 ? (
+                <div className={`medal position-${score.position}`}>
+                  <FontAwesomeIcon icon={faMedal} />
+                </div>
+              ) : (
+                <div className="position">#{score.position}</div>
+              )}
+              <div className="info first-column">
+                <span className="username">{score.userName}</span>
+                <span className="server">
+                  <span className="address">{score.server}</span>
+                  <ServerFlag server={score.server} />
+                </span>
+              </div>
+              <div className="info second-column">
+                <span>{_('content.info.score')}</span>
+                <span>{score.score}</span>
+              </div>
+              <div className="info third-column">
+                <span>
+                  <Moment format={_('content.info.day-format')}>{score.timestamp}</Moment>
+                </span>
+                <span>
+                  <Moment format={_('content.info.time-format')}>{score.timestamp}</Moment>
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <div className="ad right-side">
+          <div>
+            {[...new Array(12)].map((_, index) => (
+              <div key={index} className="banner">
+                <img src={`/${getRandomBanner()}`} alt="" />
+              </div>
+            ))}
+          </div>
+
+          <div>
+            {[...new Array(12)].map((_, index) => (
+              <div key={index} className="banner">
+                <img src={`/${getRandomBanner()}`} alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <StatusBar />
+    </div>
+  );
 }
 
 export default React.memo(HomePageDesktop);
--- src/pages/Home/breakpoints/Tablet.tsx
+++ src/pages/Home/breakpoints/Tablet.tsx
@@ -11,63 +11,82 @@ import useScores from 'src/hooks/useScores';
 import './index.css';
 import './tablet.css';
 
+const banners = [
+  'banner.webp',
+  'banner1.jpg',
+  'banner2.jpg',
+  'banner3.gif',
+  'banner4.png',
+  'banner5.gif',
+  'banner6.gif',
+  'banner7.jpg',
+];
+
+function getRandomBanner() {
+  return banners[Math.floor(Math.random() * banners.length)];
+}
+
 function HomePageTablet() {
-    const scores = useScores();
-    const { _ } = useInternationalization();
+  const scores = useScores();
+  const { _ } = useInternationalization();
 
-    return (
-        <div id="container">
-            <Header />
-            <NavBar />
-            <StatusBar />
+  return (
+    <div id="container">
+      <Header />
+      <NavBar />
+      <StatusBar />
 
-            <div id="content">
-                <div className="ad left-side">
-                    {[...new Array(12)].map(() => (
-                        <div className="banner"></div>
-                    ))}
-                </div>
-                <ul>
-                    {scores.map((score) => (
-                        <li key={score.position}>
-                            {score.position < 4 ? (
-                                <div className={`medal position-${score.position}`}>
-                                    <FontAwesomeIcon icon={faMedal} />
-                                </div>
-                            ) : (
-                                <div className="position">#{score.position}</div>
-                            )}
-                            <div className="info first-column">
-                                <span className="username">{score.userName}</span>
-                                <span className="server">
-                                    <span className="address">{score.server}</span>
-                                    <ServerFlag server={score.server} />
-                                </span>
-                            </div>
-                            <div className="info second-column">
-                                <span>{_('content.info.score')}</span>
-                                <span>{score.score}</span>
-                            </div>
-                            <div className="info third-column">
-                                <span>
-                                    <Moment format={_('content.info.day-format')}>{score.timestamp}</Moment>
-                                </span>
-                                <span>
-                                    <Moment format={_('content.info.time-format')}>{score.timestamp}</Moment>
-                                </span>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="ad right-side">
-                    {[...new Array(12)].map(() => (
-                        <div className="banner"></div>
-                    ))}
-                </div>
+      <div id="content">
+        <div className="ad left-side">
+          {[...new Array(12)].map((_, index) => (
+            <div key={index} className="banner">
+              <img src={`/${getRandomBanner()}`} alt="" />
             </div>
+          ))}
         </div>
-    );
+        <ul>
+          {scores.map((score) => (
+            <li key={score.position}>
+              {score.position < 4 ? (
+                <div className={`medal position-${score.position}`}>
+                  <FontAwesomeIcon icon={faMedal} />
+                </div>
+              ) : (
+                <div className="position">#{score.position}</div>
+              )}
+              <div className="info first-column">
+                <span className="username">{score.userName}</span>
+                <span className="server">
+                  <span className="address">{score.server}</span>
+                  <ServerFlag server={score.server} />
+                </span>
+              </div>
+              <div className="info second-column">
+                <span>{_('content.info.score')}</span>
+                <span>{score.score}</span>
+              </div>
+              <div className="info third-column">
+                <span>
+                  <Moment format={_('content.info.day-format')}>{score.timestamp}</Moment>
+                </span>
+                <span>
+                  <Moment format={_('content.info.time-format')}>{score.timestamp}</Moment>
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <div className="ad right-side">
+          {[...new Array(12)].map((_, index) => (
+            <div key={index} className="banner">
+              <img src={`/${getRandomBanner()}`} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default React.memo(HomePageTablet);
