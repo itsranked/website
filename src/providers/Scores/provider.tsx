602ee11a7490eae8cbd342bef4bd8751828bb2cf
Rafael Fontes <itsranked.net@gmail.com>
2021-04-12 21:42:35 -0300

    Change global month json location

--- src/providers/Scores/provider.tsx
+++ src/providers/Scores/provider.tsx
@@ -3,17 +3,26 @@ import ScoresContext from './context';
 import { ScoreType } from './types';
 
 function ScoresProvider(props: PropsWithChildren<{}>) {
-    const { children } = props;
+  const { children } = props;
 
-    const [scores, setScores] = useState<ScoreType[]>([]);
+  const [scores, setScores] = useState<ScoreType[]>([]);
 
-    useEffect(() => {
-        fetch('/top100-month-2021-04.json')
-            .then((response) => response.json())
-            .then((json) => setScores(json));
-    }, []);
+  const currentDate = new Date();
+  currentDate.setUTCDate(1);
+  currentDate.setUTCHours(0);
+  currentDate.setUTCMinutes(0);
+  currentDate.setUTCSeconds(0);
+  currentDate.setUTCMilliseconds(0);
 
-    return <ScoresContext.Provider value={scores}>{children}</ScoresContext.Provider>;
+  useEffect(() => {
+    fetch(
+      `/top100-global-month-${currentDate.getUTCFullYear()}-${String(currentDate.getUTCMonth() + 1).padStart(2, '0')}`,
+    )
+      .then((response) => response.json())
+      .then((json) => setScores(json));
+  }, []);
+
+  return <ScoresContext.Provider value={scores}>{children}</ScoresContext.Provider>;
 }
 
 export default React.memo(ScoresProvider);
